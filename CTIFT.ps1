e63aaa7e1f38f8d913f0e484f86f836e7ccf598c
Brian <bvw20002010@gmail.com>
2023-09-18 09:50:19 -0400

    Add files via upload

--- CTIFT.ps1
+++ CTIFT.ps1
@@ -21,7 +21,6 @@ function File_Handling {
 	# lines usually start with either number, 'd', 't'
 
 	# regex is hell
-	$pattern = "^(d|t|s)*\d{1,3}\.* *-* *((dt*\d{1,2}\.* *|t*\d{1,2}\.* *|s*\d{1,2}\.* *| *)*\d{1,2}\.*)* *-* *(\/)*"
 	$times = "(\(|\[)*\d{1,2}:\d{1,2}.\d{0,2}(\)|\])*"
 
 	# for any other patterns to remove
@@ -29,22 +28,32 @@ function File_Handling {
 
 	# TODO - add case for "dusbourne? weird tracklisting"
 
-	if ($folderName -match ".*(dusborne|GEMS).*") {
-		$pattern = "^(\d{3})* *-* *gd\d{2}-\d{2}-\d{2}(d|s)\d{1,2}t\d{2,3}:* *-* *"
-		$trimmed = $content.trim() -match $pattern -replace "$pattern", "" -replace " *-*>", " $segue_arrow" -replace "tuning", "Tuning" -replace "intro", "Intro" -replace "\s+", " " -replace "(%|;|\*+|\/\/)", ""
+	if ($folderName -match ".*dusborne.*") {
+		$pattern = "^\d{3} - (gd)\d{2}-\d{2}-\d{2}(d|s)\d{1}(t)\d{2} - "
+		$trimmed = $content.trim() -match $pattern -notmatch "Source Matrix" -replace $pattern, "" -replace " *-*>", " $segue_arrow" -replace "tuning", "Tuning" -replace "intro", "Intro" -replace "\s+", " " -replace "(%|;|\*+|\/\/)", ""
 	} else {
+		$pattern = "^([dts]|)\d{1,3}-\d{1,2}\. -* *(([dts]*\d{1,2}\. *| *)*\d{1,2}\.)* *-* *(\/)*"
 		$trimmed = $content.trim() -match $pattern -notmatch $other -replace "($pattern|$times|$other)", "" -replace " *-*>", " $segue_arrow" -replace "tuning", "Tuning" -replace "intro", "Intro" -replace "\s+", " " -replace "(%|;|\*+|\/\/)", ""
 	}
 
-	$trimmed.trim() | Out-File -FilePath "$($folderName)\$($fileName)_trimmed.txt"
+	Write-Host $trimmed
 
-	if ($outputArrowless -eq $true) {
-		$trimmed.trim() | Out-File -FilePath "$($folderName)\$($fileName)_trimmed_noArrows.txt"
-	}
+	# try {
+	# 	Write-Host $trimmed.trim()
 
-	Set-Clipboard -value "$($folderName)\$($fileName)_trimmed.txt"
-	Write-Host "Trimmed File Saved, Path Copied to Clipboard"
-	
+	# 	$trimmed.trim() | Out-File -FilePath "$($folderName)\$($fileName)_trimmed.txt"
+
+	# 	if ($outputArrowless -eq $true) {
+	# 		$trimmed.trim() | Out-File -FilePath "$($folderName)\$($fileName)_trimmed_noArrows.txt"
+	# 	}
+
+	# 	Set-Clipboard -value "$($folderName)\$($fileName)_trimmed.txt"
+	# 	Write-Host "Trimmed File Saved, Path Copied to Clipboard"
+	# }
+	# catch {
+	# 	Write-Host "ERROR: Processing Failed"
+	# }
+
 }
 
 do {
@@ -62,7 +71,3 @@ do {
 		File_Handling $args[0]
 	}
 } Until ($path -eq "0")
-
-# $path = Read-Host "`n> Enter Path to Info File (Can Also Drag and Drop File on Top of Window)"
-# $file = $path -replace "`"", ""
-# File_Handling $file
